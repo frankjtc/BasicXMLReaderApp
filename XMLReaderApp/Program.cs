a24a0a32a88d86b2c1fd772c19d05b743d3b9366
Frank J Castro <frankjtc@gmail.com>
2014-12-15 21:19:02 -0800

    first commit

---
+++ XMLReaderApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XMLReaderApp
+{
+    class XMLReaderApp :IBooksParser
+    {
+        static void Main(string[] args)
+        {
+            XmlTextReader reader = new XmlTextReader("C:\\Books.xml");
+            StringBuilder xmlString = new StringBuilder();
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+                        xmlString.Append("<" + reader.Name);
+                        xmlString.Append(">");
+                        break;
+                    case XmlNodeType.Text: //Display the text in each element.
+                        xmlString.Append(reader.Value);
+                        break;
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        xmlString.Append("</" + reader.Name);
+                        xmlString.Append(">");
+                        break;
+                }
+            }
+
+
+                XMLReaderApp app = new XMLReaderApp();
+
+                app.ParseFile(xmlString.ToString());
+                Console.ReadKey();
+            
+        }            
+
+        public void ParseFile(string fileContent) {
+
+            StringBuilder output = new StringBuilder();
+
+            // Create an XmlReader
+            using (XmlReader reader = XmlReader.Create(new StringReader(fileContent)))
+            {
+                reader.ReadToFollowing("book");
+                reader.MoveToFirstAttribute();
+                string genre = reader.Value;
+                output.AppendLine("The genre: " + genre);
+
+                reader.ReadToFollowing("title");
+                output.AppendLine("Content of the title element: " + reader.ReadElementContentAsString());
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
